c78fce91e2856798a1456e913a9e9d777eb107e6
Mago <alexandre.cabral@engenharia.ufjf.br>
2020-01-30 12:56:51 -0300

    relative paths, icons and css fixes

--- options.js
+++ options.js
@@ -26,19 +26,35 @@ document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
 
-let themes = ["styles/ambiance.css", "styles/chaos.css", "styles/chrome.css", "styles/clouds.css", 
-"styles/clouds_midnight.css", "styles/cobalt.css", "styles/crimson_editor.css", "styles/dawn.css", 
-"styles/dracula.css", "styles/dreamweaver.css", "styles/eclipse.css", "styles/github.css", "styles/gob.css", 
-"styles/gruvbox.css", "styles/idle_fingers.css", "styles/iplastic.css", "styles/katzenmilch.css", "styles/kr_theme.css", 
-"styles/kuroir.css", "styles/merbivore.css", "styles/merbivore_soft.css", "styles/monokai.css", "styles/mono_industrial.css", 
-"styles/pastel_on_dark.css", "styles/solarized_dark.css", "styles/solarized_light.css", "styles/sqlserver.css", "styles/terminal.css", 
-"styles/textmate.css", "styles/tomorrow.css", "styles/tomorrow_night.css", "styles/tomorrow_night_blue.css", "styles/tomorrow_night_bright.css", 
-"styles/tomorrow_night_eighties.css", "styles/twilight.css", "styles/vibrant_ink.css", "styles/xcode.css"];
-
-themes.forEach(item => {
-    let opt = document.createElement("option");
-    opt.value= item;
-    opt.innerHTML = item.split("/")[1].split(".")[0];
-
-    document.getElementById('theme').appendChild(opt);
+chrome.runtime.getPackageDirectoryEntry(function(directoryEntry) {
+    directoryEntry.getDirectory('styles', {}, function(subDirectoryEntry) {
+        var directoryReader = subDirectoryEntry.createReader();
+        var filenames = [];
+        (function readNext() {
+            directoryReader.readEntries(function(entries) {
+                if (entries.length) {
+                    for (var i = 0; i < entries.length; ++i) {
+                        filenames.push(entries[i].name);
+                    }
+                    readNext();
+                } else {
+                    filenames.forEach(item => {
+                        let opt = document.createElement("option");
+                        opt.value= "styles/"+item;
+                        opt.innerHTML = item.split('.')[0];
+                    
+                        document.getElementById('theme').appendChild(opt);
+                    });
+                }
+            });
+        })();
+    });
 });
+
+// themes.forEach(item => {
+//     let opt = document.createElement("option");
+//     opt.value= "styles/"+item;
+//     opt.innerHTML = files;
+
+//     document.getElementById('theme').appendChild(opt);
+// });
